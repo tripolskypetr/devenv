b7be864f2096142d0af3d5583aae0f86464ea752
Petr Tripolsky <tripolskypetr@gmail.com>
2026-08-16 00:56:21 +0500

    wip

--- scripts/linux/publish.sh
+++ scripts/linux/publish.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+set -e
 docker build --platform linux/amd64 -t tripolskypetr/devenv . -f Dockerfile
 docker push tripolskypetr/devenv:latest
--- scripts/win/publish.bat
+++ scripts/win/publish.bat
@@ -1,3 +1,7 @@
 @echo off
 call docker build --platform linux/amd64 -t tripolskypetr/devenv . -f Dockerfile
+if errorlevel 1 (
+    echo Build failed, publish cancelled.
+    exit /b 1
+)
 call docker push tripolskypetr/devenv:latest
